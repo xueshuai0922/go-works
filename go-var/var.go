663dbc9c118b787834d16cc499741fd8e5c406c7
xueshuai <xues13@chinaunicom.cn>
2022-11-22 17:01:07 +0800

    Initial commit

---
+++ go-var/var.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+// 多个变量定义的时候，变量之间必须进行换行
+var (
+	cp string
+	np int
+)
+
+// mainvar:=23//全局变量不能简要定义变量
+var mainvar string = "mainvar"
+
+func main() {
+	变量()
+	多线程()
+
+}
+
+func 多线程() {
+	for i := 0; i < 10000; i++ {
+		go fmt.Println(i)
+	}
+}
+func 变量() {
+	//第一种：
+	var name string = "xuehsuai"
+
+	fmt.Println(name)
+	//	多个变量设置
+	var sex, age = "男", "12"
+	fmt.Println(sex)
+	fmt.Println(age)
+	//简化的赋值
+	s := "xueshuai"
+	fmt.Println(s)
+	//先定义
+
+}
